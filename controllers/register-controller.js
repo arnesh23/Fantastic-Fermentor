fd3af73f2e385ca74e2f018d493d40fd6dc56f4b
Maira Vianey Jimenez Ledesma <mvjl@Mairas-MacBook-Pro.local>
2019-01-11 15:47:27 -0800

    styling update

--- controllers/register-controller.js
+++ controllers/register-controller.js
@@ -9,46 +9,7 @@ module.exports = function(app){
         db.projects.create(req.body).then(function (dbProjects) {
           res.json(dbProjects);
         });
-
-
-
-        app.delete("/api/project/:id", function (req, res) {
-          console.log('delete called')
-          var condition = "id = " + req.params.id;
-          console.log(condition)
-      
-          db.projects.destroy({
-            where: {  
-              id: req.params.id
-            }
-          }).then(function (dbTask) {
-            res.json(dbTask);
-          })
-          .catch(function (err) {
-            // handle error;
-            console.log("Error"); 
-          });
-          
-        });
-      });
-
-      app.get("/api/project/:id", function (req, res) {
-        console.log('get called')
-        var condition = "id = " + req.params.id;
-        console.log(condition)
     
-        db.projects.findOne({
-          where: {  
-            id: req.params.id
-          }
-        }).then(function (dbTask) {
-          res.json(dbTask);
-        })
-        .catch(function (err) {
-          // handle error;
-          console.log("Error"); 
-        });
-        
+    
       });
-
 }
